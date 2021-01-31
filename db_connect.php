2e5d25ade4aeca5db17d8b5da69f919c822f9405
MKianne <47920449+MKianne@users.noreply.github.com>
2021-01-31 13:16:03 +0800

    Updated database

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','bloodbank_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli('db4free.net','bloodbankdev','admin123','bloodbankdev')or die("Could not connect to mysql".mysqli_error($con));

a10516217c8ac875f6a78e4a4a1246220920c09b
MKianne <47920449+MKianne@users.noreply.github.com>
2021-02-02 11:56:15 +0800

    Removed some codes

--- home.php
+++ home.php
@@ -135,36 +135,4 @@
             }
         })
     })
-    $('#tracking_id').on('keypress',function(e){
-        if(e.which == 13){
-            get_person()
-        }
-    })
-    $('#check').on('click',function(e){
-            get_person()
-    })
-    function get_person(){
-            start_load()
-        $.ajax({
-                url:'ajax.php?action=get_pdetails',
-                method:"POST",
-                data:{tracking_id : $('#tracking_id').val()},
-                success:function(resp){
-                    if(resp){
-                        resp = JSON.parse(resp)
-                        if(resp.status == 1){
-                            $('#name').html(resp.name)
-                            $('#address').html(resp.address)
-                            $('[name="person_id"]').val(resp.id)
-                            $('#details').show()
-                            end_load()
-
-                        }else if(resp.status == 2){
-                            alert_toast("Unknow tracking id.",'danger');
-                            end_load();
-                        }
-                    }
-                }
-            })
-    }
 </script>
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <?php session_start(); ?>
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="login/images/icons/favicon.ico">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
